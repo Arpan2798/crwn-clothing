af2201d20f3df16fcb82229b670b559aaf42b6f4
arpan2798 <aksatpathi027@gmail.com>
2020-10-16 12:08:33 +0530

    put up a shop page with preview of collections

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import  Homepage  from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 function App() {
   return (
     <div>
       <switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </switch>
     </div>
   )
